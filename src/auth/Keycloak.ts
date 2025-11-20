85677ab2bad57c2b9069b781eba729726b96971d
Akarui712 <a21490574@itmexicali.edu.mx>
2025-11-19 21:04:35 -0800

    Cambio de puerto 8080

--- src/auth/Keycloak.ts
+++ src/auth/Keycloak.ts
@@ -12,7 +12,7 @@ if (!url || !realm || !clientId) {
 }
 
 const keycloak = new Keycloak({
-  url: url || 'http://localhost:8080',
+  url: url || 'https://j6t9s9cj-8080.usw3.devtunnels.ms/',
   realm: realm || 'myrealm',
   clientId: clientId || 'todo-frontend',
 });
